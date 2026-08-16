c00e16acc0f8f288753ed93c639b3bd8947c2085
Belen Escubilla <belescubilla@gmail.com>
2026-08-15 22:21:10 -0300

    cambio de tamaño del form

---
+++ Practicos/Practicos2-3/Pequeño Formulario.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Practicos.Practicos2_3
+{
+    partial class Pequeño_Formulario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.LNya = new System.Windows.Forms.Label();
+            this.LModificar = new System.Windows.Forms.Label();
+            this.LDni = new System.Windows.Forms.Label();
+            this.LApellido = new System.Windows.Forms.Label();
+            this.LNombre = new System.Windows.Forms.Label();
+            this.textDni = new System.Windows.Forms.TextBox();
+            this.textNombre = new System.Windows.Forms.TextBox();
+            this.textApellido = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(35, 227);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 0;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(137, 227);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 1;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // LNya
+            // 
+            this.LNya.AutoSize = true;
+            this.LNya.Location = new System.Drawing.Point(18, 48);
+            this.LNya.Name = "LNya";
+            this.LNya.Size = new System.Drawing.Size(92, 13);
+            this.LNya.TabIndex = 2;
+            this.LNya.Text = "Nombre y Apellido";
+            this.LNya.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // LModificar
+            // 
+            this.LModificar.AutoSize = true;
+            this.LModificar.ForeColor = System.Drawing.Color.Red;
+            this.LModificar.Location = new System.Drawing.Point(134, 48);
+            this.LModificar.Name = "LModificar";
+            this.LModificar.Size = new System.Drawing.Size(49, 13);
+            this.LModificar.TabIndex = 3;
+            this.LModificar.Text = "modificar";
+            // 
+            // LDni
+            // 
+            this.LDni.AutoSize = true;
+            this.LDni.Location = new System.Drawing.Point(18, 97);
+            this.LDni.Name = "LDni";
+            this.LDni.Size = new System.Drawing.Size(26, 13);
+            this.LDni.TabIndex = 4;
+            this.LDni.Text = "DNI";
+            // 
+            // LApellido
+            // 
+            this.LApellido.AutoSize = true;
+            this.LApellido.Location = new System.Drawing.Point(18, 136);
+            this.LApellido.Name = "LApellido";
+            this.LApellido.Size = new System.Drawing.Size(44, 13);
+            this.LApellido.TabIndex = 5;
+            this.LApellido.Text = "Apellido";
+            // 
+            // LNombre
+            // 
+            this.LNombre.AutoSize = true;
+            this.LNombre.Location = new System.Drawing.Point(18, 172);
+            this.LNombre.Name = "LNombre";
+            this.LNombre.Size = new System.Drawing.Size(44, 13);
+            this.LNombre.TabIndex = 6;
+            this.LNombre.Text = "Nombre";
+            // 
+            // textDni
+            // 
+            this.textDni.Location = new System.Drawing.Point(112, 90);
+            this.textDni.Name = "textDni";
+            this.textDni.ShortcutsEnabled = false;
+            this.textDni.Size = new System.Drawing.Size(100, 20);
+            this.textDni.TabIndex = 7;
+            this.textDni.TextChanged += new System.EventHandler(this.textDni_TextChanged);
+            this.textDni.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textDni_KeyDown);
+            this.textDni.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textDni_KeyPress);
+            // 
+            // textNombre
+            // 
+            this.textNombre.Location = new System.Drawing.Point(112, 169);
+            this.textNombre.Name = "textNombre";
+            this.textNombre.ShortcutsEnabled = false;
+            this.textNombre.Size = new System.Drawing.Size(100, 20);
+            this.textNombre.TabIndex = 8;
+            this.textNombre.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textNombre_KeyPress);
+            // 
+            // textApellido
+            // 
+            this.textApellido.Location = new System.Drawing.Point(112, 129);
+            this.textApellido.Name = "textApellido";
+            this.textApellido.ShortcutsEnabled = false;
+            this.textApellido.Size = new System.Drawing.Size(100, 20);
+            this.textApellido.TabIndex = 9;
+            this.textApellido.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textApellido_KeyPress);
+            // 
+            // Pequeño_Formulario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 361);
+            this.Controls.Add(this.textApellido);
+            this.Controls.Add(this.textNombre);
+            this.Controls.Add(this.textDni);
+            this.Controls.Add(this.LNombre);
+            this.Controls.Add(this.LApellido);
+            this.Controls.Add(this.LDni);
+            this.Controls.Add(this.LModificar);
+            this.Controls.Add(this.LNya);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnGuardar);
+            this.Name = "Pequeño_Formulario";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pequeño Formulario";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Label LNya;
+        private System.Windows.Forms.Label LModificar;
+        private System.Windows.Forms.Label LDni;
+        private System.Windows.Forms.Label LApellido;
+        private System.Windows.Forms.Label LNombre;
+        private System.Windows.Forms.TextBox textDni;
+        private System.Windows.Forms.TextBox textNombre;
+        private System.Windows.Forms.TextBox textApellido;
+    }
+}
